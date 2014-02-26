a595ce5acda821819c08c700b7d04aaee78baad6
William Kennedy <bill@ardanstudios.com>
2014-02-26 12:09:17 -0500

    Added function to round 64 bit integers

--- helper/helper.go
+++ helper/helper.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"encoding/json"
 	"fmt"
+	"math"
 	"net/url"
 	"runtime"
 
@@ -27,7 +28,8 @@ func MergeUrlValues(target *url.Values, source url.Values) {
 	}
 }
 
-//Catches Panic
+// CatchPanic is used to catch any Panic and log exceptions to Stdout.
+// It will also write the stack trace
 func CatchPanic(err *error, goRoutine string, functionName string) bool {
 	if r := recover(); r != nil {
 		// Capture the stack trace
@@ -44,4 +46,21 @@ func CatchPanic(err *error, goRoutine string, functionName string) bool {
 	}
 
 	return false
+}
+
+// Round can round up 64 it floats to any specified precision
+func Round(x float64, prec int) float64 {
+	var rounder float64
+
+	pow := math.Pow(10, float64(prec))
+	intermed := x * pow
+
+	_, frac := math.Modf(intermed)
+	if frac >= 0.5 {
+		rounder = math.Ceil(intermed)
+	} else {
+		rounder = math.Floor(intermed)
+	}
+
+	return rounder / pow
 }
