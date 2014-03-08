132e2cf88028bb04f67ae369f7dd152cb2e84278
Ed Gonzo <Ed@ArdanStudios.com>
2014-03-08 00:47:43 -0500

    changed generic err messages to lowercase

--- i18n/en-US.go
+++ i18n/en-US.go
@@ -3,14 +3,14 @@ package i18n
 var En_US = `[
 	{
 		"id": "validation_error",
-		"translation": "Validation Error: The parameters provided were invalid."
+		"translation": "validation error: the parameters provided were invalid."
 	},
 	{
 		"id": "invalid_credentials",
-		"translation": "Invalid Credentials were supplied."
+		"translation": "invalid Credentials were supplied."
 	},
 	{
 		"id": "application_error",
-		"translation": "An Application Error has occured."
+		"translation": "an application error has occured."
 	}
 ]`
